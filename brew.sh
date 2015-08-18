b24163171136cee0c0e5c858441605e523080c5f
Simon Smith <me@simonsmith.io>
2015-08-18 20:27:35 +0100

    Add latest bash

--- brew.sh
+++ brew.sh
@@ -26,6 +26,13 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --default-names
 
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
 # Install `wget` with IRI support.
 brew install wget --enable-iri
 
