fa926573d73f4d995bcd01e0c5af5ff84734208a
Simon Smith <me@simonsmith.io>
2015-09-12 08:23:41 +0100

    Add Modernizr to npmglobal

--- npmglobal.sh
+++ npmglobal.sh
@@ -1,6 +1,7 @@
 # Install node modules globally
 
-PACKAGES="caniuse-cmd bower postcss npm-check-updates gulp npmlist mversion webpack nodemon node-inspector"
+PACKAGES="caniuse-cmd bower postcss npm-check-updates gulp npmlist mversion
+          webpack nodemon modernizr"
 
 echo "Installing global npm packages: $PACKAGES"
 
