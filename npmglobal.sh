c378dc724c13c1294b4819483be4fe1487fb9d3f
Simon Smith <me@simonsmith.io>
2015-09-13 18:00:47 +0100

    Formatting

--- npmglobal.sh
+++ npmglobal.sh
@@ -1,9 +1,18 @@
 # Install node modules globally
 
-PACKAGES="caniuse-cmd bower postcss npm-check-updates gulp npmlist mversion
-          webpack nodemon modernizr"
+PACKAGES="caniuse-cmd
+          bower
+          postcss
+          npm-check-updates
+          gulp
+          npmlist
+          mversion
+          webpack
+          nodemon
+          modernizr"
 
-echo "Installing global npm packages: $PACKAGES"
+echo "Installing global npm packages:
+      $PACKAGES"
 
 npm install $PACKAGES --global --quiet
 
