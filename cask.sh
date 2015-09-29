bfbdf2cfd1c85f1bc51a585934a25555df16f345
Simon Smith <me@simonsmith.io>
2015-09-29 14:42:29 +0100

    Group casks

--- cask.sh
+++ cask.sh
@@ -5,53 +5,60 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-# Applications
-
-brew cask install appcleaner 2> /dev/null
-brew cask install bartender 2> /dev/null
-brew cask install beardedspice 2> /dev/null
-brew cask install bettertouchtool 2> /dev/null
-brew cask install burn 2> /dev/null
-brew cask install cloud 2> /dev/null
-brew cask install dropbox 2> /dev/null
-brew cask install daisydisk 2> /dev/null
-brew cask install deluge 2> /dev/null
-brew cask install firefox 2> /dev/null
-brew cask install flux 2> /dev/null
-brew cask install gitx 2> /dev/null
-brew cask install gitify 2> /dev/null
+# Browsers
 brew cask install google-chrome-beta 2> /dev/null
 brew cask install google-chrome-canary 2> /dev/null
+brew cask install firefox 2> /dev/null
+brew cask install opera 2> /dev/null
+brew cask install torbrowser 2> /dev/null
+
+# Cloud
+brew cask install cloud 2> /dev/null
+brew cask install dropbox 2> /dev/null
 brew cask install google-drive 2> /dev/null
+
+# Comms
+brew cask install slack 2> /dev/null
+brew cask install skype 2> /dev/null
 brew cask install google-hangouts 2> /dev/null
-brew cask install gpgtools 2> /dev/null
-brew cask install imageoptim 2> /dev/null
-brew cask install istat-menus 2> /dev/null
+
+# Media
+brew cask install burn 2> /dev/null
+brew cask install sonos 2> /dev/null
+brew cask install spotify 2> /dev/null
+brew cask install spotify-notifications 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install max 2> /dev/null
+brew cask install beardedspice 2> /dev/null
+
+# Development
+brew cask install gitx 2> /dev/null
+brew cask install gitify 2> /dev/null
+brew cask install mamp 2> /dev/null
+brew cask install transmit 2> /dev/null
+brew cask install vmware-fusion 2> /dev/null
 brew cask install iterm2-beta 2> /dev/null
-brew cask install imazing 2> /dev/null
-brew cask install jq 2> /dev/null
 brew cask install kaleidoscope 2> /dev/null
+brew cask install jq 2> /dev/null
+
+# Mac OS Enhancements
+brew cask install bartender 2> /dev/null
+brew cask install bettertouchtool 2> /dev/null
+brew cask install gpgtools 2> /dev/null
+brew cask install istat-menus 2> /dev/null
 brew cask install keepingyouawake 2> /dev/null
 brew cask install launchbar 2> /dev/null
 brew cask install little-snitch 2> /dev/null
-brew cask install max 2> /dev/null
-brew cask install mamp 2> /dev/null
-brew cask install 1password 2> /dev/null
-brew cask install opera 2> /dev/null
 brew cask install paragon-ntfs 2> /dev/null
-brew cask install robomongo 2> /dev/null
-brew cask install slack 2> /dev/null
 brew cask install shortcat 2> /dev/null
-brew cask install skype 2> /dev/null
-brew cask install sonos 2> /dev/null
-brew cask install spotify 2> /dev/null
-brew cask install steam 2> /dev/null
-brew cask install spotify-notifications 2> /dev/null
 brew cask install the-unarchiver 2> /dev/null
-brew cask install transmit 2> /dev/null
-brew cask install torbrowser 2> /dev/null
-brew cask install viscosity 2> /dev/null
-brew cask install vlc 2> /dev/null
-brew cask install vmware-fusion 2> /dev/null
 brew cask install xtrafinder 2> /dev/null
 
+# Other stuff
+brew cask install viscosity 2> /dev/null
+brew cask install deluge 2> /dev/null
+brew cask install flux 2> /dev/null
+brew cask install appcleaner 2> /dev/null
+brew cask install imazing 2> /dev/null
+brew cask install steam 2> /dev/null
+brew cask install daisydisk 2> /dev/null
