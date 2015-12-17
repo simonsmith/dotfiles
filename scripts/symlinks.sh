a33f8764726cd963885507bd4648805a86800ba0
Simon Smith <me@simonsmith.io>
2015-12-17 17:31:01 +0000

    Revert "Don't force symlinks"
    This reverts commit 4f2a656f9221a6a438d0638f6f578841650eb5ce.

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,10 +1,10 @@
 
 symlink_dotfile() {
-  ln -sv $(grealpath $1) "$HOME/.$1"
+  ln -sfv $(grealpath $1) "$HOME/.$1"
 }
 
 symlink() {
-  ln -sv $(grealpath $1) "$HOME/$1"
+  ln -sfv $(grealpath $1) "$HOME/$1"
 }
 
 echo "Creating symlinks ..."
