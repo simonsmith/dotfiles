6c925abba28262226049aa05422311007b233d39
Simon Smith <me@simonsmith.io>
2015-12-18 00:35:00 +0000

    Add function for installing node packages

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,14 +1,42 @@
+# Install nvm
 
+fancy_echo() {
+  local fmt="$1"; shift
+  printf "\n$fmt\n" "$@"
+}
+
+echo "Installing nvm ..."
 if [ ! -s "$NVM_DIR/nvm.sh" ] ; then
-  echo "Installing nvm ..."
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | zsh
 else
   echo "nvm already installed ..."
 fi
 
-PACKAGES="caniuse-cmd bower postcss-cli npm-upgrade gulp npmlist eslint
-stylelint stylelint-config-suitcss babel mversion webpack nodemon modernizr"
+echo "Installing npm global packages ..."
 
-echo "Installing global npm packages $PACKAGES"
+npm_packages="$(npm list -g --depth=0)"
 
-npm install $PACKAGES --global --quiet
+install_global_npm_package() {
+  if echo $npm_packages | grep $1@ > /dev/null 2>&1; then
+    fancy_echo "Already have %s installed. Skipping ..." "$1"
+  else
+    fancy_echo "Installing %s ..." "$1"
+    npm i -g "$@" --quiet
+  fi
+}
+
+install_global_npm_package 'bower'
+install_global_npm_package 'postcss-cli'
+install_global_npm_package 'postcss'
+install_global_npm_package 'eslint'
+install_global_npm_package 'stylelint'
+install_global_npm_package 'stylelint-config-suitcss'
+install_global_npm_package 'babel'
+install_global_npm_package 'webpack'
+install_global_npm_package 'mversion'
+install_global_npm_package 'nodemon'
+install_global_npm_package 'modernizr'
+install_global_npm_package 'caniuse-cmd'
+install_global_npm_package 'npm-check'
+install_global_npm_package 'gulp'
+install_global_npm_package 'npmlist'
