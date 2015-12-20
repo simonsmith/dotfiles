f60666fef9ac8f9b48ad5d15b36161b112210742
Simon Smith <me@simonsmith.io>
2015-12-20 23:55:16 +0000

    Move node lower down install list

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ source ./scripts/homebrew.sh
 source ./scripts/cask.sh
 source ./scripts/zsh.sh
 source ./scripts/symlinks.sh
-source ./scripts/node.sh
 source ./scripts/fonts.sh
 source ./scripts/vim.sh
+source ./scripts/node.sh
 source ./scripts/ruby.sh
 source ./scripts/misc.sh
 
