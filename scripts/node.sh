b97d8c66d6c33bb6200c38ae91571e75795eddd4
Simon Smith <me@simonsmith.io>
2015-12-20 18:48:05 +0000

    Install version of node and set it as default

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,6 +1,13 @@
+node_install_version=5.3.0
+
 echo "Installing nvm ..."
 if [ ! -s "$NVM_DIR/nvm.sh" ] ; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | zsh
+  source "$HOME/.nvm/nvm.sh"
+  echo "Installing node $node_install_version ..."
+  nvm install "$node_install_version"
+  nvm use "$node_install_version"
+  nvm alias default node
 else
   echo "nvm already installed ..."
 fi
