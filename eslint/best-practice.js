77baa4103899e55879e167fde59b5631ab693f00
Simon Smith <me@simonsmith.io>
2015-12-28 21:50:49 +0000

    Update ESLint style

--- eslint/best-practice.js
+++ eslint/best-practice.js
@@ -20,7 +20,6 @@ module.exports = {
     'no-floating-decimal': 2,
     'no-lone-blocks': 2,
     'no-loop-func': 2,
-    'no-magic-numbers': 2,
     'no-multi-spaces': 2,
     'no-multi-str': 2,
     'no-native-reassign': 2,
