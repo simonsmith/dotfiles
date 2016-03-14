9bcc1ebf138b5c70b6647376c8772bba5fd076a9
Simon Smith <me@simonsmith.io>
2016-03-14 11:06:57 +0000

    Use correct path for vim-plug

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -3,6 +3,6 @@ mkdir -p "$HOME/.config/nvim/autoload"
 if [ -e "$HOME/.config/nvim/autoload/plug.vim" ]; then
   log "vim plug exists, skipping ..."
 else
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo "$HOME/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
