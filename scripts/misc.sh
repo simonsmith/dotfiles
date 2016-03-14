77c3613ee473e7a78d5b2acb061394699211420a
Simon Smith <me@simonsmith.io>
2016-03-14 11:37:29 +0000

    Add log comments

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,6 +1,7 @@
 # Install fzf
+log "Setting up fzf ..."
 yes | /usr/local/opt/fzf/install
 
 # Install git-hub
-log "Installing git-hub CLI tool"
+log "Installing git-hub CLI tool ..."
 git clone https://github.com/ingydotnet/git-hub "$HOME/git-hub"
