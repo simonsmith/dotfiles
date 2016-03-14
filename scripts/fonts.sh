77b22c3bc3fea90feb61c1952f79f95e75836dcb
Simon Smith <me@simonsmith.io>
2016-03-14 11:06:50 +0000

    Install  hack font

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,20 +1,3 @@
 log "Installing fonts ..."
 
-# Check if brew uses the fonts repo
-brew tap | grep "caskroom/fonts" > /dev/null
-if [[ $? != 0 ]] ; then
-  brew tap caskroom/fonts
-fi
-
-# Checks if fonts are installed and installs them if they aren't.
-install_font() {
-  brew cask list | grep $1 > /dev/null 2>&1
-  if [[ $? != 0 ]] ; then
-    log "Installing $1 ..."
-    brew cask install $1
-  else
-    log "Skipping $1 installation ..."
-  fi
-}
-
-install_font 'font-droid-sans-mono-for-powerline'
+brew cask install caskroom/fonts/font-hack
