5ea775e486075933f2d27b27a01d8a4132d6610d
Simon Smith <me@simonsmith.io>
2016-03-14 11:28:11 +0000

    Add vim to the vim script

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,8 +1,18 @@
 mkdir -p "$HOME/.config/nvim/autoload"
+mkdir -p "$HOME/.vim/autoload"
 
+# Neovim
 if [ -e "$HOME/.config/nvim/autoload/plug.vim" ]; then
   log "vim plug exists, skipping ..."
 else
   curl -fLo "$HOME/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+fi
+
+# Vim
+if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
+  log "vim plug exists, skipping ..."
+else
+  curl -fLo "$HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
