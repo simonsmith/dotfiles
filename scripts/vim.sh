0f9e7f295d50ca89922df487fc123b17462177eb
Simon Smith <me@simonsmith.io>
2016-03-14 13:45:55 +0000

    Add fix for ctrl h mapping in neovim

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -5,14 +5,19 @@ mkdir -p "$HOME/.vim/autoload"
 if [ -e "$HOME/.config/nvim/autoload/plug.vim" ]; then
   log "vim plug exists, skipping ..."
 else
-  curl -fLo "$HOME/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Vim
 if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
   log "vim plug exists, skipping ..."
 else
-  curl -fLo "$HOME/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+# Allow CTRL-H mapping to work in neovim
+# https://github.com/neovim/neovim/wiki/Troubleshooting#my-ctrl-h-mapping-doesnt-work
+infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > $TERM.ti
+tic $TERM.ti
