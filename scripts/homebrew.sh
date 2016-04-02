9759b8081243b0d506e36922ce7fef7b0c9654b4
Simon Smith <me@simonsmith.io>
2016-04-02 22:27:57 +0100

    Add ctags to brew

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -110,6 +110,7 @@ brew_install_or_upgrade ruby-build
 # Install other useful binaries.
 brew_install_or_upgrade ack
 brew_install_or_upgrade archey
+brew_install_or_upgrade ctags
 brew_install_or_upgrade htop
 brew_install_or_upgrade heroku
 brew_install_or_upgrade httpie
