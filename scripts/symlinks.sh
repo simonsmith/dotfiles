641d8768f50d8ed8f9928e75c67fb0c2cf74238c
Simon Smith <me@simonsmith.io>
2016-04-25 10:44:25 +0100

    Revert "Create dotfile symlinks dynamically"
    This reverts commit a2f51c1a3343a08117980e0dc9f145d1bd919730.

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,3 +1,4 @@
+
 symlink_dotfile() {
   ln -sfv $(grealpath $1) "$HOME/.$1"
 }
@@ -8,12 +9,31 @@ symlink() {
 
 echo "Creating symlinks ..."
 
-# ./dots/foo -> ~/.foo
-for file in ./dots/*; do
-  ln -sfv $(grealpath $file) "$HOME/.$(basename $file)"
-done
-
+pushd ./dots
 symlink redis.conf
+symlink_dotfile ackrc
+symlink_dotfile agignore
+symlink_dotfile aliases
+symlink_dotfile caniuse.json
+symlink_dotfile ctags
+symlink_dotfile curlrc
+symlink_dotfile editorconfig
+symlink_dotfile eslintrc
+symlink_dotfile functions
+symlink_dotfile gitconfig
+symlink_dotfile gitignore
+symlink_dotfile honukai.zsh-theme
+symlink_dotfile hushlogin
+symlink_dotfile jshintrc
+symlink_dotfile stylelintrc
+symlink_dotfile tern-project
+symlink_dotfile tmux.conf
+symlink_dotfile tmuxline_snapshot
+symlink_dotfile vimrc
+symlink_dotfile wgetrc
+symlink_dotfile zshenv
+symlink_dotfile zshrc
+popd
 
 # Directories
 
