a1650ba365f929e46fc1efec7ea9d971c7196add
Simon Smith <me@simonsmith.io>
2016-05-08 15:45:48 +0100

    Simplify eslint rules
    Gets in the way of general code

--- eslint/es6.js
+++ eslint/es6.js
@@ -10,7 +10,5 @@ module.exports = {
     'arrow-body-style': [2, 'as-needed'],
     'arrow-parens': [2, 'as-needed'],
     'arrow-spacing': 2,
-    'generator-star-spacing': 2,
-    'no-confusing-arrow': 2
   }
 };
--- eslint/node.js
+++ eslint/node.js
@@ -2,9 +2,5 @@ module.exports = {
   'env': {
     'node': true,
     'mocha': true
-  },
-  'rules': {
-    'callback-return': 2,
-    'handle-callback-err': 2
   }
 };
