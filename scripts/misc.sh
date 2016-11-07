e9591debd1b73bd41ec2070a905077ce17927247
Simon Smith <simonsmith@users.noreply.github.com>
2016-11-07 15:53:11 +0000

    Add blsd install step

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -6,3 +6,7 @@ yes | /usr/local/opt/fzf/install
 # Install git-hub
 echo "Installing git-hub CLI tool ..."
 git clone https://github.com/ingydotnet/git-hub "$HOME/git-hub"
+
+# Install blsd
+# http://theinfiniteset.net/content/2016/08/combining-tools-blsd-and-fzf/
+bash <(curl -fL https://raw.githubusercontent.com/junegunn/blsd/master/install)
