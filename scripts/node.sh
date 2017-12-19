2d162f527f6ccd4cea494f29790048b3250964b5
Simon Smith <simonsmith@users.noreply.github.com>
2017-12-19 10:20:54 +0000

    Update Node version

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,11 +3,11 @@ log() {
   printf "\n\e[94m$fmt\n" "$@"
 }
 
-node_install_version=8.3.0
+node_install_version=8.9.0
 
 log "Installing nvm ..."
 if [ ! -s "$NVM_DIR/nvm.sh" ] ; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | zsh
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
   source "$HOME/.nvm/nvm.sh"
   log "Installing node $node_install_version ..."
   nvm install "$node_install_version"
