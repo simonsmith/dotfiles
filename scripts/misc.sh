b1a4d2116efe4bbfea62a67b582b68399371a0b6
Simon Smith <simonsmith@users.noreply.github.com>
2018-02-23 14:09:34 +0000

    Cleanup

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,11 +1,6 @@
-
 # Install fzf
 echo "Setting up fzf ..."
 yes | /usr/local/opt/fzf/install
-
-# Install git-hub
-echo "Installing git-hub CLI tool ..."
-git clone https://github.com/ingydotnet/git-hub "$HOME/git-hub"
 
 # Install blsd
 # http://theinfiniteset.net/content/2016/08/combining-tools-blsd-and-fzf/
--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -1,22 +1,7 @@
-log() {
-  local fmt="$1"; shift
-  printf "\n\e[94m$fmt\n" "$@"
-}
-
-gem_install_or_update() {
-  if gem list "$1" --installed > /dev/null; then
-    log "Updating %s ..." "$1"
-    gem update "$@"
-  else
-    log "Installing %s ..." "$1"
-    gem install "$@"
-    rbenv rehash
-  fi
-}
-
 ruby_version="$(curl -sSL http://ruby.thoughtbot.com/latest)"
 
-log "Installing ruby ..."
+echo "Installing ruby ..."
+
 if ! rbenv versions | grep -Fq "$ruby_version"; then
   rbenv install -s "$ruby_version"
 fi
@@ -28,11 +13,13 @@ rbenv shell "$ruby_version"
 
 gem update --system
 
-gem_install_or_update bundler
-gem_install_or_update tmuxinator
-gem_install_or_update circle-cli
+gem install bundler
+gem install tmuxinator
+gem install circle-cli
 
-log "Configuring Bundler ..."
-  number_of_cores=$(sysctl -n hw.ncpu)
-  bundle config --global jobs $((number_of_cores - 1))
-log "Bundler configured ..."
+rbenv rehash
+
+echo "Configuring Bundler ..."
+
+number_of_cores=$(sysctl -n hw.ncpu)
+bundle config --global jobs $((number_of_cores - 1))
