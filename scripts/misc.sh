f26e18471e6da35b497cb3e666ea3db4f8a4b212
Simon Smith <simonsmith@users.noreply.github.com>
2018-02-23 14:15:55 +0000

    Add cargo

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -5,3 +5,6 @@ yes | /usr/local/opt/fzf/install
 # Install blsd
 # http://theinfiniteset.net/content/2016/08/combining-tools-blsd-and-fzf/
 bash <(curl -fL https://raw.githubusercontent.com/junegunn/blsd/master/install)
+
+# Install Rust and Cargo (used for Alacritty)
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
