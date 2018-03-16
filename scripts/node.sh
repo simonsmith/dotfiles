f981cfc6e3c9689dcd1bf0901fd48b17645ec38b
Simon Smith <simonsmith@users.noreply.github.com>
2018-03-16 17:18:16 +0000

    Remove nvm install script

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,17 +1,3 @@
-node_install_version=8.9.0
-
-echo "Installing nvm ..."
-if [ ! -s "$NVM_DIR/nvm.sh" ] ; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
-  source "$HOME/.nvm/nvm.sh"
-  echo "Installing node $node_install_version ..."
-  nvm install "$node_install_version"
-  nvm use "$node_install_version"
-  nvm alias default node
-else
-  echo "nvm already installed ..."
-fi
-
 echo "Installing npm global packages ..."
 
 npm i -g --quiet eslint
