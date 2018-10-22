321b86ff4d90a6c96786775ba704285fca2ae372
Simon Smith <simonsmith@users.noreply.github.com>
2018-10-22 10:12:49 +0100

    Install ripgrep

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -27,6 +27,8 @@ symlink_dotfile gitmessage
 symlink_dotfile honukai.zsh-theme
 symlink_dotfile hushlogin
 symlink_dotfile khdrc
+symlink_dotfile ripgreprc
+symlink_dotfile rgignore
 symlink_dotfile stylelintrc
 symlink_dotfile tern-project
 symlink_dotfile tmux.conf
