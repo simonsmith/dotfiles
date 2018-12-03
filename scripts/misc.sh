c69e76e9afc528991eca82d59491a5e731e7420f
Simon Smith <simonsmith@users.noreply.github.com>
2018-12-03 19:41:05 +0000

    Add tpm

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -2,9 +2,10 @@
 echo "Setting up fzf ..."
 yes | /usr/local/opt/fzf/install
 
+# tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Install blsd
 # http://theinfiniteset.net/content/2016/08/combining-tools-blsd-and-fzf/
 bash <(curl -fL https://raw.githubusercontent.com/junegunn/blsd/master/install)
 
-# Install Rust and Cargo (used for Alacritty)
-curl -sSf https://static.rust-lang.org/rustup.sh | sh
