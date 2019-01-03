2f559598be1fc2abb0e27ddd5e2b455f7a974fa4
Simon Smith <simonsmith@users.noreply.github.com>
2019-01-03 21:28:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,5 @@ source ./scripts/node.sh
 source ./scripts/ruby.sh
 source ./scripts/misc.sh
 
-echo "Sourcing .zshrc ..."
-source "$HOME/.zshrc"
 echo "Installation complete!"
-echo "Now open a new Terminal tab to install zgen plugins..."
+echo "Now open a new Terminal tab to install zplug plugins..."
