79bca04d51ed21545465adb932593bba9a53c412
Simon Smith <simonsmith@users.noreply.github.com>
2019-01-24 12:15:08 +0000

    Switch to nvs

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,24 +1,20 @@
-node_install_version=10.15.0
+echo "Installing nvs ..."
 
-echo "Installing nvm ..."
-if [ ! -s "$NVM_DIR/nvm.sh" ] ; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | zsh
-  source "$HOME/.nvm/nvm.sh"
-  echo "Installing node $node_install_version ..."
-  nvm install "$node_install_version"
-  nvm use "$node_install_version"
-  nvm alias default node
-else
-  echo "nvm already installed ..."
-fi
+# nvs
+# https://github.com/jasongin/nvs
+# ---------------------------------------------------------------
+
+git clone https://github.com/jasongin/nvs "$HOME/.nvs"
+source "$HOME/.nvs/nvs.sh"
+nvs install lts
+nvs link lts
+nvs auto on
 
 echo "Installing npm global packages ..."
 
 npm i -g --quiet http-server
 npm i -g --quiet imageoptim-cli
-npm i -g --quiet nodemon
 npm i -g --quiet npm-check
-npm i -g --quiet npmlist
 npm i -g --quiet tern
 
 echo "Installing yarn ..."
