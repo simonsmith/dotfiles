1239609bc0cecf45de8e1b7dc5e1055b23bf1477
Simon Smith <simonsmith@users.noreply.github.com>
2021-09-28 16:06:25 +0100

    Add java sdk manager

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -9,3 +9,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # http://theinfiniteset.net/content/2016/08/combining-tools-blsd-and-fzf/
 bash <(curl -fL https://raw.githubusercontent.com/junegunn/blsd/master/install)
 
+# java version manager
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.zshrc"
+sdk install java 16.0.2.7.1-amzn
+sdk install java 11.0.12.7.2-amzn
+sdk default java 16.0.2.7.1-amzn
