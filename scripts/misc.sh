64334ebb266529b4320dcfd6deb9b537ad7502eb
Simon Smith <simonsmith@users.noreply.github.com>
2023-06-26 11:12:37 +0100

    switch to catppuccin theme

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -15,3 +15,6 @@ source "$HOME/.zshrc"
 sdk install java 17.0.0.35.2-amzn
 sdk install java 11.0.12.7.2-amzn
 sdk default java 17.0.0.35.2-amzn
+
+# catppuccin
+git clone https://github.com/catppuccin/alacritty.git ~/.config/alacritty/catppuccin
