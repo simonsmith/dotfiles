a124b7743c9932d429058536e1b10bbb85bc80dc
Simon Smith <simonsmith@users.noreply.github.com>
2026-08-24 22:22:53 +0100

    update tmux colours

--- tmux/koda-flexoki-light.sh
+++ tmux/koda-flexoki-light.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-# Flexoki Light with Koda's softened base text and session colours.
+# Koda Light surfaces with Flexoki semantic accents.
 source "$HOME/.tmux/plugins/tmux-powerkit/src/themes/flexoki/light.sh"
 
+THEME_COLORS[background]="#faf9f5"
+THEME_COLORS[statusbar-bg]="#faf9f5"
 THEME_COLORS[statusbar-fg]="#555555"
+THEME_COLORS[session-fg]="#faf9f5"
 THEME_COLORS[message-fg]="#555555"
+THEME_COLORS[message-bg]="#faf9f5"
+THEME_COLORS[popup-bg]="#faf9f5"
 THEME_COLORS[popup-fg]="#555555"
+THEME_COLORS[popup-border]="#9d7cd8"
+THEME_COLORS[menu-bg]="#faf9f5"
 THEME_COLORS[menu-fg]="#555555"
+THEME_COLORS[menu-selected-bg]="#ebebeb"
+THEME_COLORS[menu-selected-fg]="#333333"
+THEME_COLORS[menu-border]="#9d7cd8"
 THEME_COLORS[session-bg]="#9d7cd8"
+THEME_COLORS[window-inactive-base]="#ebebeb"
+THEME_COLORS[pane-border-inactive]="#ebebeb"
